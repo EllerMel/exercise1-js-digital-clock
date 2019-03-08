e8a278f1007a868e050025dab1c9415c540d4945
M Eller <msmaedev@gmail.com>
2019-03-07 20:47:20 -0500

    Refresh. JavaScript from scratch

--- web/js/main.js
+++ web/js/main.js
@@ -1,72 +1,97 @@
+//Show the current date
+//Since day and month return a number, pull full names from an array for each
 var today = new Date();
+var currDate = "";
+var showDate = document.getElementById("date");
 var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
 var yyyy = today.getFullYear();
 
-if (dd < 10) {
-  dd = '0' + dd;
-}
+var day = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-if (mm < 10) {
-  mm = '0' + mm;
-}
-document.getElementById('date').innerHTML = mm + '/' + dd + '/' + yyyy;
-document.getElementById('date').style.fontFamily = "railway";
-document.getElementById('date').style.fontSize = '100px'
+currDate = (day[today.getDay()] + ' ' + month[today.getMonth()]+ ' ' + dd + ', ' + yyyy);
+showDate.innerHTML = currDate;
+console.log(currDate);
 
-function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-  
-  function startTime() {
+//Show the current military time
+//Since minutes and seconds are a single digit under 10, add a zero
+function getMilitaryTime() {
     var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    // add a zero in front of numbers<10
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-    document.getElementById('time').style.fontFamily = "monospace";
-    document.getElementById('time').style.fontSize = "1000%";
-    t = setTimeout(function() {
-      startTime()
-    }, 500);
-  }
-  startTime();
-
-  
-  function startStandardTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-
-    if (h > 12) {
-        h -= 12;
-    } else if (h === 0) {
-        h = 12;
+    var currTime = "";
+    var showTime = document.getElementById('timeMilitary');
+    var hour = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    if (min < 10)
+    {
+        min = "0" + min
     }
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time2').innerHTML = h + ":" + m + ":" + s;
-    document.getElementById('time2').style.fontFamily = "monospace";
-    document.getElementById('time2').style.fontSize = "1000%";
-    t = setTimeout(function() {
-        startStandardTime()
-    }, 500);
-  }
-  startStandardTime();
+    if (sec < 10)
+    {
+        sec = "0" + sec
+    }
+    currTime = (hour + ':' + min + ':' + sec)
+    showTime.innerHTML = currTime;
 
-  function myFunction() {
-    var militaryTime = document.getElementById("militaryTime"); 
-    var standardTime = document.getElementById("standardTime");
- 
-    militaryTime.style.display = (
-        militaryTime.style.display == "none" ? "block" : "none"); 
-    standardTime.style.display = (
-        standardTime.style.display == "none" ? "block" : "none"); 
- }
+    setTimeout("getMilitaryTime()",500);
+}
+getMilitaryTime();
+
+//Show the current standard time
+//Account for pm hours (13+), 12am, and showing am vs pm
+//Since minutes and seconds are a single digit under 10, add a zero
+function getStandardTime() {
+    var today = new Date();
+    var currTime = "";
+    var showTime = document.getElementById('timeStandard');
+    var hour = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    var amPM = " ";
+    if (hour >= 13)
+    {
+        hour = hour - 12
+        amPM = "PM"
+    }
+    else if (hour == 0)
+    {
+        hour = hour
+        amPM = "AM"
+    }
+    else
+    {
+        hour = hour
+        amPM = "AM"
+    }
+    if (min < 10)
+    {
+        min = "0" + min
+    }
+    if (sec < 10)
+    {
+        sec = "0" + sec
+    }
+    currTime = (hour + ':' + min + ':' + sec + " " + amPM)
+    showTime.innerHTML = currTime;
+
+    setTimeout("getStandardTime()",500);
+}
+getStandardTime();
+
+//Show/hid div around each time depending on button click
+function switchTime() {
+    var x = document.getElementById("militaryTime");
+    var y = document.getElementById("standardTime");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+    if (y.style.display == "none") {
+        y.style.display = "block";
+    }
+    else {
+        y.style.display = "none";
+    }
+}
